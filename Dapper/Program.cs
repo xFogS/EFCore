facd02d69a9b8aa6ebeece1030362a117e76fdb8
xFogS <romanprotsenko4@gmail.com>
2024-04-25 23:39:49 +0300

    upd

--- Dapper/Program.cs
+++ Dapper/Program.cs
@@ -3,7 +3,6 @@ using System.Data;
 using Dapper;
 using Microsoft.Data.SqlClient;
 
-
 namespace Subscribers
 {
     public class Program
@@ -18,56 +17,33 @@ namespace Subscribers
                 "Trust Server Certificate=False;" +
                 "Application Intent=ReadWrite;" +
                 "Multi Subnet Failover=False";
-        //static IEnumerable<Subscriber> GetSubscriberFromCountry(string country)
-        //{
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        string sql = "SELECT * FROM Subscriber WHERE CountryId = (SELECT Id FROM Country WHERE Name = @Name)";
-        //        return connection.Query<Subscriber>(sql, new {Name = country});
-        //    }
-        //}
-
-        //static DataTable GetSubscrFromCountry(string country)
-        //{
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        string sql = "SELECT S.Name AS SubsName, S.Email, S.Gender, C.Name " +
-        //            "FROM Subscriber AS S " +
-        //            "JOIN Country AS C ON C.Id = S.CountryId " +
-        //            "WHERE CountryId = (SELECT Id FROM Country WHERE Name = @Name)";
-        //        var reader = connection.ExecuteReader(sql, new { Name = country });
-        //        DataTable table = new DataTable();
-        //        table.Load(reader);
-        //        return table;
-        //    }
-        //}
-
-        //static DataTable GetCountSubscrFromCountry()
-        //{
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        string sql = "SELECT C.Name AS 'Country', COUNT(S.CountryId) AS 'Count' " +
-        //            "FROM Subscriber AS S JOIN Country AS C ON S.CountryId = C.Id " +
-        //            "GROUP BY C.Name ORDER BY 2 DESC";
-        //        var reader = connection.ExecuteReader(sql);
-        //        DataTable table = new DataTable();
-        //        table.Load(reader);
-        //        return table;
-        //    }
-        //}
-
-        //static IEnumerable<Product> GetSpecialProductFromDate(string category, DateTime date)
-        //{
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        string sql = "SELECT * FROM Product AS P " +
-        //            "JOIN SpecialOfer AS SO ON P.SpecialOferId = SO.Id " +
-        //            "WHERE P.CategoryId = (SELECT Id FROM Category WHERE Name = @Category) " +
-        //            "AND (@Date BETWEEN SO.DateStart AND SO.DateEnd)";
-
-        //        return connection.Query<Product>(sql, new { Category = category, Date = date.ToString("yyyy-MM-dd") });
-        //    }
-        //}
+        //Відобразити кількість покупців у кожній країні.
+        static DataTable GetCountSubsctibersFromCountry()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT [C].[Name] AS 'Country', COUNT([S].[CountryId]) AS 'Count' FROM [Country] AS C " +
+                    "JOIN [Subscriber] AS S ON [C].[Id] = [S].[CountryId] " +
+                    "GROUP BY [C].[Name] ORDER BY COUNT([S].[CountryId]) ASC";
+                DataTable table = new DataTable();
+                table.Load(conn.ExecuteReader(sql));
+                return table;
+            }
+        }
+        static DataTable GetInterestingSubsctibersFromCountry(string country)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Category AS C " +
+                    "WHERE [C].[Id] IN (SELECT [WL].[CategoryId] FROM [WishList] AS WL " +
+                    "WHERE [WL].[SubscriberId] IN (SELECT [S].[Id] FROM [Subscriber] AS S " +
+                    "WHERE [S].[CountryId] = (SELECT [Country].[Id] FROM [Country] " +
+                    "WHERE [Country].[Name] = @Name)))";
+                DataTable table = new DataTable();
+                table.Load(conn.ExecuteReader(sql, new { Name = country }));
+                return table;
+            }
+        }
         static IEnumerable<Subscriber> GetSubscribersFromCountry(string country)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -86,12 +62,67 @@ namespace Subscribers
                 return connection.Query<SpecialOfer>(sqlSelect, new { Name = country });
             }
         }
+        static DataTable GetSpecialProductFromDate(string category, DateTime date)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM Product AS P " +
+                    "JOIN SpecialOfer AS SO ON P.SpecialOferId = SO.Id " +
+                    "WHERE P.CategoryId = (SELECT Id FROM Category WHERE Name = @Category) " +
+                    "AND (@Date BETWEEN SO.DateStart AND SO.DateEnd)";
+                DataTable table = new();
+                table.Load(connection.ExecuteReader(sql, new { Category = category, Date = date.ToString("yyyy-MM-dd") }));
+                return table;
+            }
+        }
+        static DataTable GetSpecialProductFromSubscribe(string subscribe)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM [Product] AS P " +
+                    "WHERE[P].[SpecialOferId] IS NOT NULL " +
+                    "AND[P].[SpecialOferId] IN(SELECT[SO].[Id] FROM[SpecialOfer] AS SO) " +
+                    "AND[P].[CategoryId] IN(SELECT[C].[Id] FROM[Category] AS C " +
+                    "WHERE[C].[Id] IN(SELECT[WL].[CategoryId] FROM[WishList] AS WL " +
+                    "WHERE[WL].[SubscriberId] IN(SELECT[S].[Id] FROM[Subscriber] AS S " +
+                    "WHERE[S].[Name] = @Name)))";
+                DataTable table = new();
+                table.Load(connection.ExecuteReader(sql, new { Name = subscribe }));
+                return table;
+            }
+        }
+        static DataTable GetTopCountryFromSubscribes()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT TOP(3) [C].[Name] AS 'Country', COUNT([S].[CountryId]) AS 'Count' " +
+                    "FROM[Country] AS C JOIN[Subscriber] AS S " +
+                    "ON[S].[CountryId] = [C].[Id] " +
+                    "GROUP BY[C].[Name] ORDER BY COUNT([S].[CountryId]) DESC";
+                DataTable table = new();
+                table.Load(connection.ExecuteReader(sql));
+                return table;
+            }
+        }
+        static DataTable GetTopCountry()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT TOP(1) [C].[Name] AS 'Country', COUNT([S].[CountryId]) AS 'Count' " +
+                    "FROM[Country] AS C JOIN[Subscriber] AS S " +
+                    "ON[S].[CountryId] = [C].[Id] " +
+                    "GROUP BY[C].[Name] ORDER BY COUNT([S].[CountryId]) DESC";
+                DataTable table = new();
+                table.Load(connection.ExecuteReader(sql));
+                return table;
+            }
+        }
         static void Main(string[] args)
         {
             using (SqlConnection sql = new SqlConnection(connectionString))
             {
                 {
-                    var subscriber = sql.Query<Subscriber>("SELECT * FROM [Subscriber]").ToList();
+                    /*var subscriber = sql.Query<Subscriber>("SELECT * FROM [Subscriber]").ToList();
                     var emalSubscriber = sql.Query<Subscriber>("SELECT [Email] FROM [Subscriber]").ToList();
                     var category = sql.Query<Category>("SELECT * FROM [Category]").ToList();
                     var specialOffer = sql.Query<SpecialOfer>("SELECT * FROM [SpecialOfer]").ToList();
@@ -106,7 +137,15 @@ namespace Subscribers
                     foreach (var specialoffer in getsSpecialOfferFromCountry)
                     {
                         Console.WriteLine(specialoffer.ToString());
-                    }
+                    }*/
+                }
+                {
+                    var GetCounSubFrCoun = GetCountSubsctibersFromCountry(); GetCounSubFrCoun.Print();
+                    var GetInterestSubFrCoun = GetInterestingSubsctibersFromCountry("Country 1"); GetInterestSubFrCoun.Print();
+                    var GetSpecialSubFrCoun = GetSpecialProductFromDate("Category 1", new DateTime(2024, 03, 27)); GetSpecialSubFrCoun.Print();
+                    var GetSpecialSubFrSub = GetSpecialProductFromSubscribe("Serg"); GetSpecialSubFrSub.Print();
+                    var GetTopCountryFrSub = GetTopCountryFromSubscribes(); GetTopCountryFrSub.Print();
+                    var GetTopCoun = GetTopCountry(); GetTopCoun.Print();
                 }
             }
         }
